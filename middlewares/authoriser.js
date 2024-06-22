1c9d3201040f8f42ac308980cf3152d5dbf091e7
Sumit Choudhary <sumitxae@gmail.com>
2024-06-22 13:29:53 +0530

    FIX: Deployment Authentication Fixes

--- middlewares/authoriser.js
+++ middlewares/authoriser.js
@@ -3,16 +3,12 @@ const ErrorHandler = require("../utils/errorHandler");
 const { catchAsyncError } = require("./catchAsyncErrors");
 
 exports.isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const {token}  = req.cookies;
+  let token  = req.cookies.token || req.headers.authorization.split(' ')[1];
 
-  console.log(req.cookies)
-  console.log(req)
-  if (!token) {
-    
+  if(!token) {
     return next(new ErrorHandler("Login first to access this resource", 401));
   }
   const { id } = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(id)
   req.id = id;
   next();
 });
